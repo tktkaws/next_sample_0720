7248d0e9f88a18a355f7bded469e5ec9a2a7be36
Takayuki Takahashi <ttkaws15@gmail.com>
2024-07-20 14:27:26 +0900

    Fix: テキスト修正

--- src/app/hobby/page.tsx
+++ src/app/hobby/page.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link';
 const Hobby = () => {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
-      <div className="">My Hobby</div>
-      <Link href="/">Homeへ</Link>
+      <div className="mt-20 bg-white rounded-lg shadow-md p-6">
+        <h2 className="text-xl font-bold mb-4">My Hobby</h2>
+        <p className="text-gray-600">お笑い</p>
+        <p className="text-gray-600">HIPHOP</p>
+      </div>
+      <Link href="/" className="mt-20">Homeへ</Link>
     </div>
   );
 }
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link';
 const Work = () => {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
-      <div className="">My Work</div>
-      <Link href="/">Homeへ</Link>
+      <div className="mt-20 bg-white rounded-lg shadow-md p-6">
+        <h2 className="text-xl font-bold mb-4">My Work</h2>
+        <p className="text-gray-600">Webサイト制作</p>
+        <p className="text-gray-600">アクセシビリティ対応</p>
+      </div>
+      <Link href="/" className="mt-20">Homeへ</Link>
     </div>
   );
 }
